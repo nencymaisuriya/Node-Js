5c31594c609d3acf42315f0b5af762563de0be26
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2025-02-24 22:30:53 +0530

    Delete 1 prg calculator directory

--- 1 prg calculator/index.js
+++
@@ -1,9 +0,0 @@
-const cal = require("./cal");
-
-const a = 21;
-const b = 3;
-
-console.log(`Addition: ${cal.add(a, b)}`);
-console.log(`Subtraction: ${cal.sub(a, b)}`);
-console.log(`Multiplication: ${cal.mul(a, b)}`);
-console.log(`Division: ${cal.div(a, b)}`);
